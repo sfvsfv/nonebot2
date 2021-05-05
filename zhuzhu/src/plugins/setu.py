38f4be0826c427bc9bb3e48abacd7506e6369270
chuanchuan <62045791+89461561511656@users.noreply.github.com>
2021-05-05 14:09:54 +0800

    插件

---
+++ zhuzhu/src/plugins/setu.py
@@ -0,0 +1,30 @@
+"""
+作者：yanghanwen
+时间：2021/5/5
+"""
+from nonebot.adapters.cqhttp import Message
+from nonebot import on_keyword
+from nonebot.typing import T_State
+from nonebot.adapters import Bot, Event
+import requests, re
+from aiocqhttp.exceptions import Error as CQHttpError
+
+yulu = on_keyword({'涩图'},priority=10)
+@yulu.handle()
+async def j(bot: Bot, event: Event, state: T_State):
+    msg = await mei()
+    print(msg)
+    try:
+        await yulu.send(Message(msg))
+
+    except CQHttpError:
+        pass
+
+
+async def mei():
+    url = 'https://api.66mz8.com/api/rand.img.php?type=美女&format=json'
+    resp = requests.get(url)
+    data = resp.json()
+    ur = data.get('pic_url')
+    tu = f"[CQ:image,file={ur}]"
+    return tu
