4ca02dc634a97929fdf1265c40027376918cc152
chuanchuan <62045791+89461561511656@users.noreply.github.com>
2021-08-13 04:56:34 +0800

    定时demo

---
+++ zhuzhu/src/plugins/dingshi.py
@@ -0,0 +1,13 @@
+from nonebot import require
+import nonebot
+# import time
+# scheduler = require('nonebot_plugin_apscheduler').scheduler
+# @scheduler.scheduled_job('cron', hour='*/1', id='sleep_sched')
+# async def sch():
+#     d = time.strftime("%m-%d %H:%M:%S", time.localtime())
+#     bot=nonebot.get_bots()['653211616']
+#     return await bot.call_api('send_group_msg',**{
+#         'message':'持续为你报时：目前时间为：{}点'.format(d),
+#         'group_id':'970353786'
+#     })
+
